909cae068c598519edb2f1fb762f9e66c1f68174
jibbs1703 <abrahamjibbs1703@gmail.com>
2025-11-25 23:10:28 -0500

    test upload package to s3 - scm

--- mypackage/__init__.py
+++ mypackage/__init__.py
@@ -1,3 +1,6 @@
 """A simple test package for uploading to private PyPI."""
 
-__version__ = "0.1.0"
+try:
+    from mypackage._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
